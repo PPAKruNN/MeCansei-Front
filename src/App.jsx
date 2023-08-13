b73ba9c8d5a44f84f4bd50dd54491eb5ccc76ce0
ppakrunn notebook <sn.jaypee@gmail.com>
2023-08-13 17:16:56 -0300

    Pages: Login / Cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { styled } from 'styled-components'
+import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <StyledApp>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Logo</div>
+        <div>Menu</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <Login/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </StyledApp>
   )
 }
+
+const StyledApp = styled.div`
+  & > div:nth-child(1) {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100px;
+    background-color: #f1f1f1;
+  }
+
+  & > div:nth-child(2) {
+    display: flex;
+    padding: 100px 0px;
+    justify-content: center;
+    width: 100%;
+  }
+
+`
 
 export default App
